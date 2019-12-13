51d9e388a7948fdc81feb72fa52758ea07a42c61
Victor Espina <vespinas@gmail.com>
2019-12-13 16:24:26 -0300

    Merge manual con PrecisaTech (2)

--- PrecisaFrozen.Win/Formularios/Otros/Frm_Principal.vb
+++ PrecisaFrozen.Win/Formularios/Otros/Frm_Principal.vb
@@ -31,7 +31,7 @@ Public Class Frm_Principal
         '       DEFINIMOS EL TITULO DE LA VENTANA PRINCIPAL INCLUYENDO
         '       EL NOMBRE DE LA BD ACTIVA
         '
-        Me.Text = "Order By WMS - Precisa Tech 13.12.19.16.52 - BD: " + CONFIG.mainCatalog
+        Me.Text = "Order By WMS - Precisa Tech 13.12.19.16.55 - BD: " + CONFIG.mainCatalog
         InfoUsuario.Text = "001"
         If Not My.Computer.Name = " PROGRAMACION-PC" Then
             Presentacion.ShowDialog()
